3a73d786b3066d47d2189e0dd68a6d5bd1b57c74
first_name <ss329222@gmail.com>
2022-02-26 18:23:11 +0530

    Added:AddLastMethod

--- src/com/blz/Day14/LinkedList.java
+++ src/com/blz/Day14/LinkedList.java
@@ -1,24 +1,66 @@
-/**UC1 : PROBLEM STATEMENT
- * create a s Linked List of 56, 30 and 70
- */
-
+/*
+* @purpose :Ability to create Linked List by adding 30 and 56 to 70 - Node with data 70 And add first method,
+  Add last method
+ LinkedList Sequence: 56->30->70
+*  Node with data 70 is First Created
+	- Next 30 is added to 70
+	- Finally 56 is added to 30
+	- 
+	 */
 package com.blz.Day14;
 
 public class LinkedList {
-	public static void main(String[] args) {
-		System.out.println("Welcome to  LinkedList Datastructure Program");
-		Node firstNode = new Node(56);
-		Node secondNode = new Node(30);
-		Node thirdNode = new Node(70);
-		Node head = firstNode;
-		firstNode.next = secondNode;
-		secondNode.next = thirdNode;
-		Node tail = thirdNode;
+	Node head;
 
-		Node temp = head;
-		while (temp != null) {
-			System.out.println(temp.data);
-			temp = temp.next;
+	class Node {
+		Object data;
+		Node next;// next is a reference
+
+		public Node(Object data) {
+			this.data = data;
 		}
 	}
+	// add first method
+
+	public void addFirst(Object data) {
+		Node newNode = new Node(data);
+		if (head == null)
+			head = newNode;
+		else {
+			newNode.next = head;
+			head = newNode;
+		}
+	}
+
+	// display linked list method
+
+	public void display() {
+		if (head == null)
+			System.out.println("No elements to display");
+		else {
+			Node temp = head;
+			while (temp.next != null) {
+				System.out.print(temp.data + " -> ");
+				temp = temp.next;
+			}
+			System.out.println(temp.data);
+		}
+	}
+
+	// AddLast
+	public void addLast(Object data) {
+		Node newNode = new Node(data);
+		if (head == null)
+			head = newNode;
+		else if (head.next == null)
+			head.next = newNode;
+		else {
+			Node temp = head;
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
+		}
+	}
+
 }
